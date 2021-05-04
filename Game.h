26e6a7cb597b85b4b6c88c23fb921e17ce7202e1
ImFakerPT <81787295+ImFakerPT@users.noreply.github.com>
2021-05-04 18:00:43 +0100

    Update Game.h

--- Game.h
+++ Game.h
@@ -7,20 +7,23 @@ using namespace std;
 class Game
 {
 	private:
-//	Jogador* player1, * player2; // Na casse Jogador devem guardar os tiros dados e os seus barcos/navios
-		Player P1, P2;
-
+		Player P;
+		Bot B;
 	public:
 		Game();
 		virtual ~Game();
 		void NewGame();
 		void Demo();
-		void Save(std::string file,Bot P);
-		void Save(std::string file,Player oponent);
-		void Read(std::string file,Bot P);
-
-		
+		void LastGame();
+		void Info();
+		void SavePlayer(std::string file);
+		void SaveBot(std::string file);
+		void ReadPlayer(std::string file);
+		void ReadBot(std::string file);
+		//void ReadBotShots(std::string file);
 };		
+	
+
 	
 
 
